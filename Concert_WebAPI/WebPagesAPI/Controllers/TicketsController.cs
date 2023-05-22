47eedb1e7ef39e88a556331953b439a4d546e9ca
andreskps <pipesierra146@gmail.com>
2023-05-22 11:51:14 -0500

    Add view validate ticket

---
+++ Concert_WebAPI/WebPagesAPI/Controllers/TicketsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPagesAPI.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        public TicketsController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public IActionResult Validate() 
+        {
+            ViewBag.Message = TempData["Message"];
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Validate(Guid? id, string entraceGate)
+        {
+            try
+            {
+
+                var url = "https://localhost:7147/api/Tickets/Validate/" + id + "/" + entraceGate;
+                var json = await _httpClient.CreateClient().PutAsync(url, null);
+                var result = await json.Content.ReadAsStringAsync();
+
+                TempData["Message"] = result;
+
+            }
+            catch (Exception ex)
+            {
+                TempData["Message"] = ex.Message;
+            }
+
+
+            return RedirectToAction("Validate");
+
+        }
+    }
+}
---
+++ Concert_WebAPI/WebPagesAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebPagesAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
